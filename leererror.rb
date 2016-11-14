f1cb8dffd651c776b18b1740af53aa4d0a3dae0b
codeamac06 <codeamac06@gmail.com>
2016-11-14 16:41:02 -0600

    nuevo

--- leererror.rb
+++ leererror.rb
@@ -8,33 +8,27 @@ end
 # Lee el método y comprende que hace y que resultado regresa 
 
 def max_letter_frequency_per_word(sentence)
-    sentence.split.select{|word| word.length > 3}.map{ |w| w.count("e") }.max
+    numbers_larger_than(letter_per_word(words_longer_than(sentence, 3), "e"))
 end
 
-def words_longer_than(array,num)
-  words = []
-  # count_of_array = array.split.length
-  # if count_of_array > num
-  #   word << count_of_array 
+def words_longer_than(sentence,num)
+  sentence.split.select{|word| word.length > num}
+  # words = []
+  # array.each do |word|
+  #   if word.length > num
+  #     words << word
+  #   end
   # end
-  # word
-  array.each do |word|
-    if word.length > num
-      words << word
-    end
-  end
-  words
+  # words
 end
 
 def letter_per_word(array,letter)
-  counts = []
-  array.each do |words|
-    counts << words.count("#{letter}")
-  end
-  counts
-  # array.split.map do |words| 
-  #   words.count("#{letter}")
-  # end  
+  array.map{ |w| w.count("e") }
+  # counts = []
+  # array.each do |words|
+  #   counts << words.count("#{letter}")
+  # end
+  # counts 
 end
 
 def numbers_larger_than(num_array)
@@ -45,8 +39,3 @@ end
 # Antes y después de modificar los métodos anteriores estas lineas deben de imprimir "true"
 puts dummy_encrypt("EsteEsMiPassword") == "DROWSSApImS*ETS*"
 puts max_letter_frequency_per_word("entero entrar envase enviar enzima equino equipo erario erguir eriaza eriazo erigir eringe eficientemente electroencefalografía") == 5
-sentence = "entero entrar envase enviar enzima equino equipo erario erguir eriaza eriazo erigir eringe eficientemente electroencefalografía" 
-puts max_letter_frequency_per_word(sentence) #== 5
-p words = words_longer_than(sentence.split, 3) #== 5
-p letters = letter_per_word(words,"e") 
-p numbers_larger_than(letters) 
