0a1c220a41d7dc08b0adb88dc4168d9003c82c41
codeamac06 <codeamac06@gmail.com>
2016-11-14 19:01:53 -0600

    joao

---
+++ Irb.rb
@@ -0,0 +1,54 @@
+CodeaCamp06s-Mac-mini:lunes codea_mac_06$ irb
+irb(main):001:0> jhxgfhjsg
+NameError: undefined local variable or method `jhxgfhjsg' for main:Object
+  from (irb):1
+  from /Users/codea_mac_06/.rbenv/versions/2.2.4/bin/irb:11:in `<main>'
+irb(main):002:0> 
+CodeaCamp06s-Mac-mini:lunes codea_mac_06$ irb
+irb(main):001:0> 7+5
+=> 12
+irb(main):002:0> 9-4
+=> 5
+irb(main):003:0> 10.5 - 11
+=> -0.5
+irb(main):004:0> "sss"
+=> "sss"
+irb(main):005:0> "sss" + "lll"
+=> "ssslll"
+irb(main):006:0> a = "string"
+=> "string"
+irb(main):007:0> a.capitalize
+=> "String"
+irb(main):008:0> array = ["a", "b", "c"]
+=> ["a", "b", "c"]
+irb(main):009:0> print array
+["a", "b", "c"]=> nil
+irb(main):010:0> puts array
+a
+b
+c
+=> nil
+irb(main):011:0> p array
+["a", "b", "c"]
+=> ["a", "b", "c"]
+irb(main):024:0> array
+=> ["a", "b", "c"]
+irb(main):025:0> array.inspect
+=> "[\"a\", \"b\", \"c\"]"
+irb(main):007:0> a.capitalize
+=> "String"
+irb(main):008:0> array = ["a", "b", "c"]
+=> ["a", "b", "c"]
+irb(main):034:0> b = 7690939204
+=> 7690939204
+
+irb(main):041:0> Math.sqrt(b)
+=> 87698.0
+
+irb(main):048:0> def shout(string)
+irb(main):049:1> string.upcase
+irb(main):050:1> end
+=> :shout
+irb(main):051:0> shout("hola")
+=> "HOLA"
+irb(main):052:0> 
---
+++ prueba2.rb
@@ -0,0 +1,6 @@
+require 'prime'
+
+Prime.each(3) do |prime| 
+ p prime
+ # string_array << prime
+end 
